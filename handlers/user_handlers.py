af4f3547fbe46a4771ce4abed7f2071fe5af0104
Henrik - Laptop <HenrikDransfeld@gmx.net>
2014-05-08 17:56:05 +0200

    Errorhandling serverseitig

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -2,11 +2,13 @@ import webapp2
 import cgi, logging
 
 import time
+import string
 import webapp2_extras.appengine.auth.models as auth_models
 
 from webapp2_extras import security, json
 import webapp2_extras
-
+from httplib import BAD_REQUEST
+from models.Error import Error, ErrorCode
 
 class BaseHandler(webapp2.RequestHandler):
     
@@ -15,17 +17,18 @@ class BaseHandler(webapp2.RequestHandler):
 
     def store(self):
         return auth_models.auth.get_store()
+    
+    def sendJson(self, obj):
+        self.response.out.write(json.encode(obj))
 
 
 class PlayerHandler(BaseHandler):
     
     def get(self):
-        return
+        self.response.out.write("Hallo Welt")
+    
     def post(self):
-        
-        
         client_user = json.decode(self.request.body);
-        logging.critical(str(client_user))
         
         name = client_user['Name']
         password = client_user['Password']
@@ -36,18 +39,22 @@ class PlayerHandler(BaseHandler):
                                                               mail=mail, 
                                                               raw_password=password
                                                               )
-        
-        
         if success:
-            id = user.get_id()
-            auth_token = self.store().user_model().create_auth_token(id)
-        
-        
-            user_info = {'id': id, 
-                         'auth_token': auth_token}
-        
-            self.response.write(json.encode(user_info))
+            user_info = self._createUserInfo(user)
+            self.sendJson(user_info)
         else:
-            self.response.out.write(json.encode(user))
-            self.response.set_status(409)
+            error = Error(ErrorCode.NOT_UNIQUE, "The following fields are already in use: "+ string.join(user, ','))
+            self.response.out.write(json.encode(error.__dict__))
+            self.response.set_status(BAD_REQUEST)
+        
+    def _createUserInfo(self, user):
+        id = user.get_id()
+        auth_token = self.store().user_model().create_auth_token(id)
+        return {'id': str(id), 'auth_token': auth_token }
+    
+    def _createNotUniqueError(self, propertiesThatAreNotUnique):
+        
+        error = Error(ErrorCode.NOT_UNIQUE, "The following fields are already in use: "+ string.join(user, ','))
+        
+        
         
